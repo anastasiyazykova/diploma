1a59ad700839ba7412d9ecfd5f3b325324eac141
Anastasiya Zykova <azalekss@mail.ru>
2021-04-17 13:08:05 +0700

    seek commit

--- static/main.js
+++ static/main.js
@@ -6,11 +6,16 @@ function newEl(tag, parent){
 	return el;
 }
 
+GUESTION = 3
+
 function generateQuestion(){
 	var fields = [
 		'Age', 'Gender', 'Name', 'Female', 'Banana', 'You are?', 'Agropequaria'
 	];
-	fields = fields.map(field => ({'name': field}));
+	fields = fields.map(field => ({
+		name: field,
+		type: field.length < 5 ? 0 : 1
+	}));
 	return {fields, action: 'saveQuestion'};
 }
 
@@ -23,13 +28,18 @@ async function request(data){
 }
 
 async function onSubmit(fields){
-	var req = fields.map(field => ({id: field.id, data: field.value.value}));
-	req = await request({action: 'saveAnswer', fields: req, questionId: 1});
+	var req = [];
+	console.log(fields);
+	for(var k in fields){
+		req.push({id: +k, data: fields[k]});
+	}
+	console.log(req);
+	req = await request({action: 'saveAnswer', fields: req, questionId: GUESTION});
 	location.reload();
 }
 
 async function main(){
-	var {fields} = await request({action: 'getQuestion', questionId: 1});
+	var {fields} = await request({action: 'getQuestion', questionId: GUESTION});
 	for(var field of fields){
 		var name = newEl('span', cont);
 		name.classList.add('name');
@@ -44,4 +54,43 @@ async function main(){
 	but.onclick = () => onSubmit(fields);
 }
 
-window.onload = () => main();
+async function main2(){
+	Survey
+		.StylesManager
+		.applyTheme("modern");
+	var {fields} = await request({action: 'getQuestion', questionId: GUESTION});
+	console.log(fields);
+	var json = {
+		completedHtml: 'Yeah',
+		pages: [],
+		showQuestionNumbers: 'off'
+	};
+	var groups = {
+		group0: {
+			name: 'group0',
+			elements: []
+		},
+		group1: {
+			name: 'group1',
+			elements: []
+		}
+	};
+	json.pages = [groups.group0, groups.group1];
+	for(var i = 0; i < fields.length; i++){
+		var group = groups[`group${fields[i].type}`];
+		group.elements.push({
+				"type": "rating",
+				"name": '' + fields[i].id,
+				"title": fields[i].name
+		})
+	}
+	window.survey = new Survey.Model(json);
+	survey
+		.onComplete
+		.add(function (result) {
+			onSubmit(result.data);
+		});
+	$("#surveyElement").Survey({model: survey});
+}
+
+window.onload = () => main2();
