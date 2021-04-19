ee462b53c4b8993442a664c3ab7c3fa2993b9064
Chernik <chernik.nsk.ru@mail.ru>
2021-04-19 14:28:25 +0700

    new

--- static/main.js
+++ static/main.js
@@ -6,16 +6,20 @@ function newEl(tag, parent){
 	return el;
 }
 
-GUESTION = 3
+GUESTION = 3;
 
 function generateQuestion(){
 	var fields = [
-		'Age', 'Gender', 'Name', 'Female', 'Banana', 'You are?', 'Agropequaria'
+		{
+			type: 'mark',
+			title: 'Лекторы хороши?'
+		},
+		{
+			type: 'mark',
+			title: 'Лекторы хороши?'
+		}
+		
 	];
-	fields = fields.map(field => ({
-		name: field,
-		type: field.length < 5 ? 0 : 1
-	}));
 	return {fields, action: 'saveQuestion'};
 }
 
@@ -29,32 +33,14 @@ async function request(data){
 
 async function onSubmit(fields){
 	var req = [];
-	console.log(fields);
 	for(var k in fields){
 		req.push({id: +k, data: fields[k]});
 	}
-	console.log(req);
 	req = await request({action: 'saveAnswer', fields: req, questionId: GUESTION});
 	location.reload();
 }
 
 async function main(){
-	var {fields} = await request({action: 'getQuestion', questionId: GUESTION});
-	for(var field of fields){
-		var name = newEl('span', cont);
-		name.classList.add('name');
-		name.innerText = field.name;
-		var value = newEl('input', cont);
-		value.classList.add('value');
-		field.value = value;
-	}
-	var but = newEl('button', cont);
-	but.classList.add('submit');
-	but.innerText = 'Submit!';
-	but.onclick = () => onSubmit(fields);
-}
-
-async function main2(){
 	Survey
 		.StylesManager
 		.applyTheme("modern");
@@ -62,35 +48,27 @@ async function main2(){
 	console.log(fields);
 	var json = {
 		completedHtml: 'Yeah',
-		pages: [],
+		pages: [{
+			name: 'page1',
+			elements: fields.map(field => {
+				if (field.data.type=='mark') {
+					return {
+						type: "rating",
+            			name: '' + field.id,
+						title: field.data.title,
+						minRateDescription: "1",
+						maxRateDescription: "5"
+					}
+				}
+			})
+		}],
 		showQuestionNumbers: 'off'
 	};
-	var groups = {
-		group0: {
-			name: 'group0',
-			elements: []
-		},
-		group1: {
-			name: 'group1',
-			elements: []
-		}
-	};
-	json.pages = [groups.group0, groups.group1];
-	for(var i = 0; i < fields.length; i++){
-		var group = groups[`group${fields[i].type}`];
-		group.elements.push({
-				"type": "rating",
-				"name": '' + fields[i].id,
-				"title": fields[i].name
-		})
-	}
 	window.survey = new Survey.Model(json);
 	survey
 		.onComplete
-		.add(function (result) {
-			onSubmit(result.data);
-		});
+		.add(result => onSubmit(result.data));
 	$("#surveyElement").Survey({model: survey});
 }
 
-window.onload = () => main2();
+window.onload = () => main();
